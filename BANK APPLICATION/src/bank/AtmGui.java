2dc3da1c1157c48d29ff2a28d567ee23d130610c
jeeva-sudhan <68820035+jeeva-sudhan@users.noreply.github.com>
2022-04-21 18:19:16 +0530

    Delete BANK APPLICATION directory

--- BANK APPLICATION/src/bank/AtmGui.java
+++
@@ -1,7 +0,0 @@
-package bank;
-//PICTURISE GUI SCREEN IN ATM
-public interface AtmGui
-{
-	public void displayScreen(); //to communicate with customer
-	  
-}
--- BANK APPLICATION/src/bank/NormalBankFunction.java
+++
@@ -1,9 +0,0 @@
-package bank;
-//COMMOIN BANK FUNCTION PROVIDED BY ALL BANKS
-public interface NormalBankFunction 
-{
-	public void deposit();
-	public void withDraw();
-	public void checkBalance();
-	public void transferMoney();
-}
